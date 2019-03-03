44b8b56d505c788278a8a7d8a337686078c108bd
Vojtech Juranek <vojtech.juranek@gmail.com>
2019-03-03 13:14:02 +0100

    Skip creating DB and user

--- scias/scias-db/initdb-01-create_db.sql
+++
@@ -1,7 +0,0 @@
--- psql -U postgres
-
-CREATE USER scias WITH PASSWORD 'scias';
-
--- malaria analysis database
-CREATE DATABASE scias_malaria WITH OWNER scias;
-GRANT ALL PRIVILEGES ON DATABASE scias_malaria to scias;
